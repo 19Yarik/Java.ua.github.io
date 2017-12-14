2cd0bbe986494ae507bcdb80af15cce26dc83f02
Java.ua <34554678+19Yarik@users.noreply.github.com>
2017-12-15 01:52:48 +0200

    Add files via upload

---
+++ formdata.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // наш код
+}
+
+
+$name = $_GET['name']; 
+
+
+
+$paswrd = $_GET['paswrd']; 
+
+
+
+
+echo $name."<br />".$paswrd."<br />
+
+?>
+
+
+
